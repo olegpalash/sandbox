bc19038dcdd057fa7b92bdb2eac0e261222e4fd8
Oleg Palash <oleg.palash.98@gmail.com>
2018-08-11 23:55:13 +0300

    Fixed falling speed

--- source/engine.d
+++ source/engine.d
@@ -3,21 +3,33 @@ import cell;
 
 class Engine
 {
-	private Map  _map;
-	private uint _t;
+	private Map      _map;
+	private uint     _t;
+	private bool[][] _used;
 
 	this(Map m)
 	{
-		_map = m;
-		_t   = 0;
+		_map  = m;
+		_t    = 0;
+		_used = new bool[][](m.height, m.width);
 	}
 
 	void update()
 	{
+		foreach(line; _used)
+		{
+			foreach(ref v; line)
+			{
+				v = false;
+			}
+		}
+
 		for (int i = 0; i < _map.height-1; i++)
 		{
 			for (int j = 0; j < _map.width; j++)
 			{
+				if (_used[i][j]) continue;
+
 				auto c = _map[i, j];
 
 				if (c.type == CellType.powder)
@@ -25,18 +37,21 @@ class Engine
 					if (_map[i+1, j].type == CellType.empty)
 					{
 						_map.swapCells(i, j, i+1, j);
+						_used[i+1][j] = true;
 					}
 					else if (j > 0 && 
 						left() &&
 						_map[i+1, j-1].type == CellType.empty)
 					{
 						_map.swapCells(i, j, i+1, j-1);
+						_used[i+1][j-1] = true;
 					}
 					else if (j < _map.width-1 && 
 						right() &&
 						_map[i+1, j+1].type == CellType.empty)
 					{
 						_map.swapCells(i, j, i+1, j+1);
+						_used[i+1][j+1] = true;
 					}
 				}
 			}
