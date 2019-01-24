fd8ac1e7ba98f5a3ee64a52490488d898254137b
Oleg Palash <oleg.palash.98@gmail.com>
2019-01-24 02:39:10 +0200

    Change algorithm in Engine.update

--- source/engine.d
+++ source/engine.d
@@ -30,73 +30,74 @@ class Engine
 			{
 				if (_used[i][j]) continue;
 
-				auto c = _map[i, j];
+				Cell c = _map[i, j];
 
 				if (c.type == CellType.powder)
 				{
-					if (_map[i+1, j].type == CellType.empty ||
-						_map[i+1, j].type == CellType.liquid)
+					if (left())	
 					{
-						_map.swapCells(i, j, i+1, j);
-						_used[i+1][j] = true;
+						moveCell(i, j, powderVecL);
 					}
-					else if (j > 0 && 
-						left() &&
-						(_map[i+1, j-1].type == CellType.empty ||
-						 _map[i+1, j-1].type == CellType.liquid))
+					else
 					{
-						_map.swapCells(i, j, i+1, j-1);
-						_used[i+1][j-1] = true;
-					}
-					else if (j < _map.width-1 && 
-						right() &&
-						(_map[i+1, j+1].type == CellType.empty ||
-						 _map[i+1, j+1].type == CellType.liquid))
-					{
-						_map.swapCells(i, j, i+1, j+1);
-						_used[i+1][j+1] = true;
+						moveCell(i, j, powderVecR);
 					}
 				}
 				else if (c.type == CellType.liquid)
 				{
-					if (_map[i+1, j].type == CellType.empty)
+					if (left())	
 					{
-						_map.swapCells(i, j, i+1, j);
-						_used[i+1][j] = true;
+						moveCell(i, j, liquidVecL);
 					}
-					else if (j > 0 && 
-						left() &&
-						_map[i+1, j-1].type == CellType.empty)
+					else
 					{
-						_map.swapCells(i, j, i+1, j-1);
-						_used[i+1][j-1] = true;
-					}
-					else if (j < _map.width-1 && 
-						right() &&
-						_map[i+1, j+1].type == CellType.empty)
-					{
-						_map.swapCells(i, j, i+1, j+1);
-						_used[i+1][j+1] = true;
-					}
-					else if (j > 0 && 
-						left() &&
-						_map[i, j-1].type == CellType.empty)
-					{
-						_map.swapCells(i, j, i, j-1);
-						_used[i][j-1] = true;
-					}
-					else if (j < _map.width-1 && 
-						right() &&
-						_map[i, j+1].type == CellType.empty)
-					{
-						_map.swapCells(i, j, i, j+1);
-						_used[i][j+1] = true;
+						moveCell(i, j, liquidVecR);
 					}
 				}
 			}
 		}
 
 		_t++;
+	}
+
+	private void moveCell(int i, int j, int[][] vec)
+	{
+		foreach (v; vec)
+		{
+			int ti = i+v[0];
+			int tj = j+v[1];
+
+			if (tj < 0 || tj >= _map.width)
+			{
+				continue;
+			}
+
+			Cell c = _map[i, j];
+
+			if (displaces(c, _map[ti, tj]))
+			{
+				_map.swapCells(i, j, ti, tj);
+				_used[ti][tj] = true;
+				break;
+			}
+		}
+	}
+
+	private bool displaces(ref Cell c1, ref Cell c2)
+	{
+		if (c2.type == CellType.empty) 
+		{
+			return c1.type == CellType.powder ||
+				c1.type == CellType.liquid;
+		}
+		else if (c2.type == CellType.liquid)
+		{
+			return c1.type == CellType.powder;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	private bool left()
@@ -108,4 +109,9 @@ class Engine
 	{
 		return _t%2 != 0;
 	}
+
+	private enum powderVecL = [[1, 0], [1, -1], [1,  1]];
+	private enum powderVecR = [[1, 0], [1,  1], [1, -1]];
+	private enum liquidVecL = powderVecL ~ [[0, -1], [0,  1]];
+	private enum liquidVecR = powderVecR ~ [[0,  1], [0, -1]];
 }
